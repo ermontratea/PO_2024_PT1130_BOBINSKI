94c65912e80db236da31a3ec4cb66e7d08da0881
ermontratea <148285232+ermontratea@users.noreply.github.com>
2025-01-21 05:45:57 +0100

    Delete oolab/src/main/java/agh/ics/oop/model/exceptions directory

--- oolab/src/main/java/agh/ics/oop/model/exceptions/IncorrectPositionException.java
+++
@@ -1,9 +0,0 @@
-package agh.ics.oop.model.exceptions;
-
-import agh.ics.oop.model.Vector2d;
-
-public class IncorrectPositionException extends Exception{
-    public IncorrectPositionException(Vector2d position){
-        super("Position " + position + " is not correct.");
-        ;}
-}
